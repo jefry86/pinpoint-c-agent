3088747da3d4069138f0689a1529151dde5db517
满鸿 <manhong@xin.com>
2018-12-27 11:47:55 +0800

    更新测试APP DEMO

--- quickstart/php/web/interceptor.php
+++ quickstart/php/web/interceptor.php
@@ -44,8 +44,52 @@ echo "PDOStatement::execute 1 done.<br/>";
 $sth->execute(array(':sn' => 'BJ'));
 echo "PDOStatement::execute 2 done.<br/>";
 
-//curl
+//mysql
+$link = mysql_connect('172.16.98.17:3306', 'xin', '48sdf37EB7');
+echo "mysql_connect done.<br/>";
 
+mysql_select_db ('xin', $link);
+echo "mysql_select_db done.<br/>";
+
+mysql_query('SELECT * FROM `city_copy` WHERE `shortname`="BJ"', $link);
+echo "mysql_query done.<br/>";
+
+mysql_close($link);
+
+//mysqli
+$mysqli = new mysqli('172.16.98.17:3306', 'xin', '48sdf37EB7', 'xin');
+echo "mysqli::__construct done.<br/>";
+
+$mysqli->query('SELECT * FROM `city_copy` WHERE `shortname`="BJ"');
+echo "mysqli::query done.<br/>";
+
+$stmt = $mysqli->prepare('SELECT * FROM `city_copy` WHERE `shortname`=?');
+echo "mysqli::prepare done.<br/>";
+
+$city = 'AA';
+$stmt->bind_param('s', $city);
+echo "mysqli_stmt::bind_param done.<br/>";
+
+$stmt->execute();
+echo "mysqli_stmt::execute done.<br/>";
+
+//mysql func
+$link = mysqli_connect("172.16.98.17:3306", "xin", "48sdf37EB7", "xin");
+echo "mysqli_connect done.<br/>";
+
+mysqli_query($link, 'SELECT * FROM `city_copy` WHERE `shortname`="BJ"');
+echo "mysqli_query done.<br/>";
+
+$stmt = mysqli_prepare($link, 'SELECT * FROM `city_copy` WHERE `shortname`=?');
+echo "mysqli_prepare done.<br/>";
+
+mysqli_stmt_bind_param($stmt, "s", $city);
+echo "mysqli_stmt_bind_param done.<br/>";
+
+mysqli_stmt_execute($stmt);
+echo "mysqli_stmt_execute done.<br/>";
+
+//curl
 $ch = curl_init();
 echo "curl_init done.<br/>";
 
